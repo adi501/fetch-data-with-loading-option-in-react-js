d8cbbbb998559cbd509954373f1d8ae2c1e9a999
Adinarayana Reddy JC <jc.adi101@gmail.com>
2020-04-06 18:32:45 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# fetch-data-with-loading-option-in-react-js
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+import FetchDataLoading from "./Components/FetchDataLoading";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FetchDataLoading />{" "}
+    </div>
+  );
+}
